f3e120959f8029406b7149481d2e270d7437c436
Dmitri Gordeev <dimapushkin@gmail.com>
2021-05-27 21:01:44 +0300

    Initial Commit

---
+++ WeekdayFinder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeekdayFinder
+//
+//  Created by Повелитель on 27.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
